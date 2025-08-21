26e775a538d4105a57dfa12aa352bb63f6b8a037
arena-belajar <septaranja@gmail.com>
2025-08-21 18:26:58 +0700

    lirik masih gagal

---
+++ easy/lyrics/main-lyrics.py
@@ -0,0 +1,16 @@
+import sys
+from rich import print
+from time import sleep
+import time
+
+def printLyrics():
+    lines = [
+        ("La-la-la-la", 0.06), #1
+        ("Ri-ri-ras, ri-ras-ra-ra-ti-ti-ti-ta", 0.06), #1
+        ("Ra-ri-ras, ra-ra--ti-ti-ti-tas-ti-ti", 0.06), #1
+        ("Rastis! Rastis! Ra-ti-ti-la!", 0.06), #1
+        ("Let's start a new life from the darkness", 0.06), #1
+        ("Until the light reveals the end", 0.06), #1
+        ("Sinister faces, growing curses", 0.06), #1
+        ("This is my last war", 0.06), #1
+    ]
